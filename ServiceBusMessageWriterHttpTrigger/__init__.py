bfa334296ba8b1843a52afa028da046b8c83eaea
Manaloto <johnmanaloto@chevron.com>
2021-12-21 09:48:55 -0700

    json payload

--- ServiceBusMessageWriterHttpTrigger/__init__.py
+++ ServiceBusMessageWriterHttpTrigger/__init__.py
@@ -1,12 +1,13 @@
 import logging
-
+import json
 import azure.functions as func
 
 
 def main(req: func.HttpRequest, msg: func.Out[str]) -> func.HttpResponse:
   
-    input_msg = req.params.get('message')
+    logging.info(req.get_json())
+    input_msg = req.get_json()
 
-    msg.set(input_msg)
+    msg.set(json.dumps(input_msg))
 
     return 'OK'
